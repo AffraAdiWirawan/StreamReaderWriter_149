ad34dbabace7b763bd4877f54d3c3722374d094f
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2023-06-19 15:03:14 +0700

    memnbuat metode untuk menuju nama file dan sekali metode while ketika benar

--- ioFile/ioFile.cpp
+++ ioFile/ioFile.cpp
@@ -8,5 +8,19 @@ int main() {
 
 	//membuka file dalam mode menulis.
 	ofstream outfile;
+	//menuju ke sebuah nama file
+	outfile.open("contohfile.txt");
+
+	cout << ">=Menulis file, \'q'\ untuk keluar" << endl;
+
+	while (true) {
+		cout << "-";
+		//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika anda memasukan karakter q
+		if (baris == "q")break;
+		//menulis dan memasukan nilai  dari 'baris' ke dalam file
+		outfile << baris << endl;
+	}
 
 }
