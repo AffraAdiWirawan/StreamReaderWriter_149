ba2d9616b34b3667c37ceeba959d920aa61fa5b5
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2023-06-19 15:11:25 +0700

    metode buka file dan menujk

--- ioFilewithPath/ioFilewithPath.cpp
+++ ioFilewithPath/ioFilewithPath.cpp
@@ -6,4 +6,10 @@ using namespace std;
 int main() {
 	string baris;
 	string NamaFile;
+
+	// membuka file dalam mode menulis.
+	ofstream outfile;
+	// menujuk ke sebuah nama file
+	outfile.open(NamaFile, ios::out);
+
 }
