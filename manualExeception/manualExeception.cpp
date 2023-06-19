9fd17a2775639f6326afbab719eb1fdd6b6b2e0c
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2023-06-19 15:15:42 +0700

    metode try di output

--- manualExeception/manualExeception.cpp
+++ manualExeception/manualExeception.cpp
@@ -3,5 +3,11 @@ using namespace std;
 
 int main()
 {
-	
+
+	try {
+		cout << "selamat belajar di prodi ti umy " << endl;
+		// throw 0,5; //melemparkan sebuah integer maka
+		cout << " pernyataan tidak akan diexsekusi" << endl;
+	}
+
 }
