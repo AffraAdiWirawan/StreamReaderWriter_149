dea355c4ad0b56996bf2c3fac0ba993ff0604976
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2023-06-19 15:16:39 +0700

    mteode selain int akan di eksekusi

--- manualExeception/manualExeception.cpp
+++ manualExeception/manualExeception.cpp
@@ -13,5 +13,11 @@ int main()
 		//blok ini akan diesekusi
 		cout << " pengecualian akan diexsekusi" << endl;
 	}
+	catch (....) {
+		//jika selain integer maka block ini akan dieksekusi
+		cout << "default pengecualina diexsekusi " << endl;
+
+	}
+	return 0;
 
 }
