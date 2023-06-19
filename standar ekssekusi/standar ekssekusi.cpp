ebfe4e11284075c2dde6961fd75dafd2ad3e63db
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2023-06-19 15:19:10 +0700

    metode untuk penyimpanan data

---
+++ standar ekssekusi/standar ekssekusi.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <exception>
+#include <array>
+using namespace std;
+
+int main(){
+	cout << " awal program " << endl; // penanda 1:..
+	try {
+		array <int, 3> data = { 1,2,3 };
+		//pesan array integer 3 elemen
+		// cout << data.at(5)<< endl;
+		// memanggil array elemen ke 5
+	}
+	
+}
